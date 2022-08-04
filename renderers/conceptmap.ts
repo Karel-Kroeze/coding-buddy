83c70192f16505d0cfec4ac2308723e9f34d5964
Karel A. Kroeze <k.a.kroeze@utwente.nl>
2022-08-04 11:48:13 +0200

    fix minimum size of conceptmap rendered

--- renderers/conceptmap.ts
+++ renderers/conceptmap.ts
@@ -1,16 +1,23 @@
-import { Network, DataSet } from 'vis';
-import { visOptions } from './visOptions';
+import { Network, DataSet } from "vis";
+import { visOptions } from "./visOptions";
 declare const artifact: any;
 
-function createNetwork( map: any ){
-    console.log({map});
-    let nodes = new DataSet( map.nodes );
-    let edges = new DataSet( map.edges );
-    let network = new Network( document.getElementById("artifact")!, { nodes, edges }, visOptions );
-    console.log( { map, nodes, edges, network, visOptions } );
+function createNetwork(map: any) {
+    console.log({ map });
+    let nodes = new DataSet(map.nodes);
+    let edges = new DataSet(map.edges);
+    let networkElement = document.getElementById("artifact")!;
+    let network = new Network(networkElement!, { nodes, edges }, visOptions);
+    networkElement.style.minHeight = "500px";
+    console.log({ map, nodes, edges, network, visOptions });
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    console.log( createNetwork( artifact.content.map || artifact.content.content || artifact.content.cmap.map ) );
+document.addEventListener("DOMContentLoaded", () => {
+    console.log(
+        createNetwork(
+            artifact.content.map ||
+                artifact.content.content ||
+                artifact.content.cmap.map
+        )
+    );
 });
-
